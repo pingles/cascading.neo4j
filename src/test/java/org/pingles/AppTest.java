d481d842f4cf291806925e2bf5f7fa746bbef8d5
Paul Ingles <paul@oobaloo.co.uk>
2012-10-30 14:42:49 +0000

    every journey... added simple test to run from intellij

--- src/test/java/org/pingles/AppTest.java
+++ src/test/java/org/pingles/AppTest.java
@@ -1,38 +1,15 @@
 package org.pingles;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import static junit.framework.Assert.assertEquals;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+@RunWith(JUnit4.class)
+public class AppTest {
+    @Test
+    public void shouldSucceed() {
+        assertEquals("a", "a");
     }
 }
