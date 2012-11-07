4d9f6f11bf6e266d1d525a9d6e3cc0f9d668e861
Paul Lam <paul@quantisan.com>
2012-11-07 16:44:08 +0000

    recordwriter will now batch records in chunks. add some logging.

--- src/main/java/org/pingles/cascading/neo4j/hadoop/Neo4jOutputFormat.java
+++ src/main/java/org/pingles/cascading/neo4j/hadoop/Neo4jOutputFormat.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.OutputFormat;
 import org.apache.hadoop.mapred.RecordWriter;
-import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.util.Progressable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +13,8 @@ import java.io.IOException;
 public class Neo4jOutputFormat<K, V> implements OutputFormat<K,V> {
     private static final Logger LOGGER = LoggerFactory.getLogger(Neo4jOutputFormat.class);
     public static final String REST_CONN_STRING_CONFIG_PROPERTY = "org.pingles.neo4j.connection.string";
+    private static final String DEFAULT_BATCH_SIZE = "10";
+    public static final String BATCH_SIZE_PROPERTY_NAME = "org.pingles.neo4j.batch_size";
 
     public static void setNeo4jConnectionString(JobConf conf, String restConnectionString) {
         conf.set(REST_CONN_STRING_CONFIG_PROPERTY, restConnectionString);
@@ -25,7 +26,9 @@ public class Neo4jOutputFormat<K, V> implements OutputFormat<K,V> {
         String restConnectionString = jobConf.get(REST_CONN_STRING_CONFIG_PROPERTY);
         LOGGER.info("Creating RecordWriter, connecting to {}", restConnectionString);
 
-        return new Neo4jRecordWriter(restConnectionString);
+        int batchSize = Integer.valueOf(jobConf.get(BATCH_SIZE_PROPERTY_NAME, DEFAULT_BATCH_SIZE));
+
+        return new Neo4jRecordWriter(restConnectionString, batchSize);
     }
 
     private void setNeo4jJavaClientSystemProperties(JobConf jobConf) {
--- src/main/java/org/pingles/cascading/neo4j/hadoop/Neo4jRecordWriter.java
+++ src/main/java/org/pingles/cascading/neo4j/hadoop/Neo4jRecordWriter.java
@@ -8,32 +8,75 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Properties;
 
 public class Neo4jRecordWriter<K, V extends Neo4jWritable> implements RecordWriter<K, V> {
     private static final Logger LOGGER = LoggerFactory.getLogger(Neo4jRecordWriter.class);
     private RestGraphDatabase database;
     private Transaction transaction;
     private String restConnectionString;
+    private int recordsInBatch = 0;
+    private long numberOfCommittedTransactions = 0;
+    private final int batchSize;
 
-    public Neo4jRecordWriter(String restConnectionString) {
+    public Neo4jRecordWriter(String restConnectionString, int batchSize) {
         this.restConnectionString = restConnectionString;
-        LOGGER.info("Creating Neo4jRecordWriter to connect to {}", restConnectionString);
+        this.batchSize = batchSize;
+
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("Creating Neo4jRecordWriter to connect to {}", restConnectionString);
+            LOGGER.info("Batch size: {}", getBatchSize());
+        }
     }
 
     public void write(K k, V v) throws IOException {
-        if (transaction == null) {
-            transaction = database().beginTx();
-        }
+        ensureTransaction();
         v.store(database);
+        recordsInBatch++;
+    }
+
+    private void ensureTransaction() {
+        if (shouldCreateNewTransaction()) {
+            if (existingTransaction()) {
+                commitTransaction();
+            }
+            transaction = database().beginTx();
+            if (LOGGER.isInfoEnabled()) {
+                LOGGER.info(String.format("Created new transaction: %s", transaction.getClass().toString()));
+            }
+        }
     }
 
     public void close(Reporter reporter) throws IOException {
-        if (transaction != null) {
-            transaction.success();
-            transaction.finish();
+        if (existingTransaction()) {
+            commitTransaction();
         }
         database().shutdown();
+
+        reporter.incrCounter("org.pingles.cascading.neo4j", "Batch transactions committed", numberOfCommittedTransactions);
+
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info(String.format("Closing writer. Committed %d transactions.", numberOfCommittedTransactions));
+        }
+    }
+
+    private boolean shouldCreateNewTransaction() {
+        return transaction == null || recordsInBatch % getBatchSize() == 0;
+    }
+
+    private boolean existingTransaction() {
+        return (transaction != null);
+    }
+
+    private void commitTransaction() {
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info(String.format("Committing transaction with %d records.", recordsInBatch));
+        }
+
+        transaction.success();
+        transaction.finish();
+
+        numberOfCommittedTransactions++;
+        recordsInBatch = 0;
     }
 
     private RestGraphDatabase database() {
@@ -41,5 +84,9 @@ public class Neo4jRecordWriter<K, V extends Neo4jWritable> implements RecordWrit
             database = new RestGraphDatabase(restConnectionString);
         }
         return database;
+    }
+
+    private int getBatchSize() {
+        return batchSize;
     }
 }
